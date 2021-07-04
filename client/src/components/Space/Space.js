cf089031ec80292ed30d107d26a8da175b4ea088
modimanju <modimanju2019@gmail.com>
2021-07-04 17:28:55 +0530

    fetching user details & displaying

--- client/src/components/Space/Space.js
+++ client/src/components/Space/Space.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { auth } from '../../firebase'
 import { useHistory } from 'react-router-dom';
+import { useAuth } from "../../contexts/AuthContext"
 
 const Space = () => {
     const history = useHistory();
-    console.log(auth);
+    const { user } = useAuth();
+    console.log(user);
     const handleLogout = async () => {
         await auth.signOut();
         history.push('/')
     }
     return (
         <div>
+            <h1>Welcome {user.displayName}</h1>
             <button onClick={handleLogout}>Logout</button>
         </div>
     )
